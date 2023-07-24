11f2b641e152ccabc5ab464e792e57b082e883e1
Miles Wilson <miles.wilson@intimetec.com>
2023-07-24 13:25:55 -0600

    Added styles to profile screen

--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -1,8 +1,23 @@
+import * as React from 'react';
 import {FC} from 'react';
-import {Text} from 'react-native';
+import {Image, Text, View} from 'react-native';
+import {User} from '../../api/users';
+import {styles} from './profile.styles';
 
-const Profile: FC = () => {
-  return <Text>Profile!</Text>;
+const Profile: FC = ({navigation, route}) => {
+  const user: User = route.params.user;
+  return (
+    <View>
+      <Image style={styles.avatar} source={{uri: user.avatar}} />
+      <Text>@{user.username}</Text>
+      <Text>
+        {user.first_name} {user.last_name}
+      </Text>
+      <Text>{user.employment.title}</Text>
+      <Text>{user.employment.key_skill}</Text>
+      <Text>{user.phone_number}</Text>
+    </View>
+  );
 };
 
 export default Profile;
---
+++ src/screens/profile/profile.styles.ts
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  avatar: {
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+});

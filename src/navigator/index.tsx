b72a95454bb0002e349e4253fdfaef877386f147
Miles Wilson <miles.wilson@intimetec.com>
2023-07-24 10:59:35 -0600

    Created separate folder structure for screens

--- src/navigator/index.tsx
+++ src/navigator/index.tsx
@@ -1,30 +1,31 @@
 import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Button, Text} from 'react-native';
+import Users from '../screens/users';
+import Profile from '../screens/profile';
 
-const HomeScreen: React.FC = ({navigation}) => {
-  return (
-    <Button
-      title="Go to Jane's profile"
-      onPress={() => navigation.navigate('Profile', {name: 'Jane'})}
-    />
-  );
-};
-const ProfileScreen: React.FC = ({navigation, route}) => {
-  return <Text>This is {route.params.name}'s profile</Text>;
-};
+// const HomeScreen: React.FC = ({navigation}) => {
+//   return (
+//     <Button
+//       title="Go to Jane's profile"
+//       onPress={() => navigation.navigate('Profile', {name: 'Jane'})}
+//     />
+//   );
+// };
+// const ProfileScreen: React.FC = ({navigation, route}) => {
+//   return <Text>This is {route.params.name}'s profile</Text>;
+// };
 
 const Stack = createNativeStackNavigator();
 
 const MyStack = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator initialRouteName="Users">
       <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{title: 'Welcome'}}
+        name="Users"
+        component={Users}
+        options={{title: 'All Users'}}
       />
-      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Profile" component={Profile} />
     </Stack.Navigator>
   );
 };
---
+++ src/screens/users/index.tsx
@@ -0,0 +1,8 @@
+import {FC} from 'react';
+import {Text} from 'react-native';
+
+const Users: FC = () => {
+  return <Text>Users</Text>;
+};
+
+export default Users;

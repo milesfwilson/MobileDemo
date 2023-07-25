a89d4691dc30a3d9a786287b997468c696c976fb
Miles Wilson <miles.wilson@intimetec.com>
2023-07-25 14:08:42 -0600

    Styled profile screen

--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -1,22 +1,96 @@
 import * as React from 'react';
 import {FC} from 'react';
-import {Image, Text, View} from 'react-native';
+
+import {
+  Image,
+  Linking,
+  Platform,
+  Pressable,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
 import {User} from '../../api/users';
 import {styles} from './profile.styles';
+import {globalStyles} from '../../index.styles';
 
 const Profile: FC = ({navigation, route}) => {
   const user: User = route.params.user;
+
+  const handleEmail = () => {
+    Linking.openURL('mailto:' + user.email);
+  };
+  const handlePhone = () => {
+    Linking.openURL('sms:' + user.phone_number);
+  };
+
+  const handleAddress = async () => {
+    const destination = encodeURIComponent(
+      `${user.address.street_address} ${user.address.zip_code}, ${user.address.city}`,
+    );
+    const provider = Platform.OS === 'ios' ? 'apple' : 'google';
+    const link = `http://maps.${provider}.com/?daddr=${destination}`;
+    try {
+      const supported = await Linking.canOpenURL(link);
+
+      if (supported) {
+        Linking.openURL(link);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <View>
-      <Image style={styles.avatar} source={{uri: user.avatar}} />
-      <Text>@{user.username}</Text>
-      <Text>
-        {user.first_name} {user.last_name}
-      </Text>
-      <Text>{user.employment.title}</Text>
-      <Text>{user.employment.key_skill}</Text>
-      <Text>{user.phone_number}</Text>
-    </View>
+    <ScrollView>
+      <View style={[styles.contactCard, globalStyles.boxShadow]}>
+        <View style={[styles.avatar, globalStyles.boxShadow]}>
+          <Text style={[styles.avatarText, globalStyles.boxShadow]}>
+            {user.first_name[0]}
+            {user.last_name[0]}
+          </Text>
+        </View>
+        <Text style={globalStyles.header}>
+          {user.first_name} {user.last_name}
+        </Text>
+        <Text>{user.employment.title}</Text>
+        <View style={[styles.contactMethods]}>
+          <Text style={globalStyles.header}>Email</Text>
+          <Pressable
+            onPress={handleEmail}
+            style={({pressed}) => [
+              styles.pressable,
+              globalStyles.row,
+              pressed && styles.pressed,
+            ]}>
+            <Text>{user.email}</Text>
+          </Pressable>
+          <Text style={globalStyles.header}>Phone</Text>
+          <Pressable
+            onPress={handlePhone}
+            style={({pressed}) => [
+              styles.pressable,
+              globalStyles.row,
+              pressed && styles.pressed,
+            ]}>
+            <Text>{user.phone_number}</Text>
+          </Pressable>
+          <Text style={globalStyles.header}>Address</Text>
+          <Pressable
+            onPress={handleAddress}
+            style={({pressed}) => [
+              styles.pressable,
+              pressed && styles.pressed,
+            ]}>
+            <Text>{user.address.street_address}</Text>
+            <Text>
+              {user.address.city}, {user.address.state} {user.address.zip_code}
+            </Text>
+            <Text>{user.address.country}</Text>
+          </Pressable>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
--- src/screens/profile/profile.styles.ts
+++ src/screens/profile/profile.styles.ts
@@ -1,11 +1,48 @@
 import {StyleSheet} from 'react-native';
+import {globalColors} from '../../index.styles';
 
 export const styles = StyleSheet.create({
   avatar: {
-    height: 100,
-    width: 100,
-    borderRadius: 50,
+    height: 180,
+    width: 180,
+    borderRadius: 90,
+    borderWidth: 5,
+    borderColor: globalColors.lighter,
+    backgroundColor: globalColors.primary,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  avatarText: {
+    fontSize: 75,
+    color: globalColors.white,
+  },
+  contactCard: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: globalColors.white,
+    margin: 15,
+    padding: 20,
+    borderRadius: 10,
+  },
+  contactMethods: {
+    marginTop: 15,
+    width: '100%',
+  },
+  pressable: {
+    padding: 20,
+    borderRadius: 10,
     borderWidth: 1,
-    borderColor: 'black',
+    borderColor: globalColors.secondary,
+    marginBottom: 20,
+    marginTop: 10,
+    backgroundColor: globalColors.white,
+  },
+  pressed: {
+    backgroundColor: globalColors.light,
   },
 });
